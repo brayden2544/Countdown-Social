7f1a32fee92b817e70d3af7d49e5a3d2f50e7616
brayden2544 <brayden2544@gmail.com>
2014-04-27 14:48:25 -0600

    Initial Commit

---
+++ Countdown Social/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Countdown Social
+//
+//  Created by Brayden Adams on 4/27/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Countdown SocialTests/Countdown_SocialTests.m
@@ -0,0 +1,34 @@
+//
+//  Countdown_SocialTests.m
+//  Countdown SocialTests
+//
+//  Created by Brayden Adams on 4/27/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Countdown_SocialTests : XCTestCase
+
+@end
+
+@implementation Countdown_SocialTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
