e0f48feb6153c85991df84425c6df54f2e6b165d
brayden2544 <brayden2544@gmail.com>
2014-06-03 14:58:52 -0400

    New Navigation Controller with Parallax type Menu

---
+++ Countdown Social/MenuViewController.m
@@ -0,0 +1,64 @@
+//
+//  MenuViewController.m
+//  Countdown Social
+//
+//  Created by Brayden Adams on 5/21/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import "MenuViewController.h"
+#import "RESideMenu.h"
+
+@interface MenuViewController ()
+
+@end
+
+@implementation MenuViewController
+
+
+
+
+-(void)awakeFromNib{
+    self.contentViewController =[self.storyboard instantiateViewControllerWithIdentifier:@"contentViewController"];
+    self.leftMenuViewController =[self.storyboard instantiateViewControllerWithIdentifier:@"leftMenuViewController"];
+    self.rightMenuViewController =[self.storyboard instantiateViewControllerWithIdentifier:@"rightMenuViewController"];
+    self.backgroundImage = [UIImage imageNamed:@"Stars"];
+
+    self.delegate =self;
+}
+
+#pragma mark -
+#pragma mark RESideMenu Delegate
+
+- (void)sideMenu:(RESideMenu *)sideMenu willShowMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"willShowMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+- (void)sideMenu:(RESideMenu *)sideMenu didShowMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"didShowMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+- (void)sideMenu:(RESideMenu *)sideMenu willHideMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"willHideMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+- (void)sideMenu:(RESideMenu *)sideMenu didHideMenuViewController:(UIViewController *)menuViewController
+{
+    NSLog(@"didHideMenuViewController: %@", NSStringFromClass([menuViewController class]));
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
