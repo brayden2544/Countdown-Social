9cf4b07abb04a8a3df0b997f936cdf7bc9fdb8e9
brayden2544 <brayden2544@gmail.com>
2014-07-18 12:20:45 -0600

    Settings View Controller Menu Added

---
+++ Countdown Social/SettingsViewController.m
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.m
+//  Countdown Social
+//
+//  Created by Brayden Adams on 7/18/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+#import "RESideMenu.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationController.navigationBarHidden = YES;
+}
+
+
+
+
+- (IBAction)presentMenu:(id)sender {
+    [self.sideMenuViewController presentLeftMenuViewController];
+}
+@end
