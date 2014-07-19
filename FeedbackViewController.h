4251d60bde93da6f0542cdaa5515daadda26227e
brayden2544 <brayden2544@gmail.com>
2014-07-18 18:54:37 -0600

    Feedback View Controller Added

---
+++ FeedbackViewController.h
@@ -0,0 +1,15 @@
+//
+//  FeedbackViewController.h
+//  Countdown Social
+//
+//  Created by Brayden Adams on 7/18/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedbackViewController : UIViewController<UITextViewDelegate>
+- (IBAction)presentMenu:(id)sender;
+- (IBAction)submitFeedback:(id)sender;
+
+@end
---
+++ FeedbackViewController.m
@@ -0,0 +1,101 @@
+//
+//  FeedbackViewController.m
+//  Countdown Social
+//
+//  Created by Brayden Adams on 7/18/14.
+//  Copyright (c) 2014 Brayden Adams. All rights reserved.
+//
+
+#import "FeedbackViewController.h"
+#import "RESideMenu.h"
+#import <FacebookSDK/FacebookSDK.h>
+#import "User.h"
+#import "AppDelegate.h"
+
+
+@interface FeedbackViewController ()
+@property (strong, nonatomic) IBOutlet UITextView *feedbackTextView;
+@property (strong, nonatomic) NSString *feedback;
+
+@end
+
+@implementation FeedbackViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationController.navigationBarHidden = YES;
+    self.feedbackTextView.delegate = self;
+    
+    
+    
+}
+
+- (void)postFeedback{
+    User *obj = [User getInstance];
+    NSDictionary *user = obj.user;
+    NSString *urlAsString =@"http://api-dev.countdownsocial.com/user/";
+    urlAsString = [urlAsString stringByAppendingString:[[user objectForKey:@"uid"] stringValue]];
+    urlAsString =[urlAsString stringByAppendingString:@"/note"];
+    
+    FBSession *session = [(AppDelegate *)[[UIApplication sharedApplication] delegate] FBsession];
+    NSString *FbToken = [session accessTokenData].accessToken;
+    
+    
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager.requestSerializer setValue:FbToken forHTTPHeaderField:@"Access-Token"];
+    NSOperationQueue *backgroundQueue = [[NSOperationQueue alloc]init];
+    manager.operationQueue = backgroundQueue;
+    NSDictionary *params = @{@"note":_feedback};
+    [manager POST:urlAsString parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Feedback Uploaded!!"
+                                                        message:@"Thanks for helping us make Countdown better!"
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"You're Welcome", nil];
+        [alert show];
+        
+    }
+          failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         NSLog(@"Error: %@", error);
+         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Feedback Not Uploaded!!"
+                                                         message:@"Please try again!"
+                                                        delegate:self
+                                               cancelButtonTitle:@"OK"
+                                               otherButtonTitles:nil];
+         [alert show];
+         
+     }];
+    
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        NSLog(@"Clicked button index 0");
+    }
+}
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    
+    if([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (IBAction)presentMenu:(id)sender {
+    [self.sideMenuViewController presentLeftMenuViewController];
+}
+
+- (IBAction)submitFeedback:(id)sender {
+    _feedback =_feedbackTextView.text;
+    [self postFeedback];
+}
+@end
