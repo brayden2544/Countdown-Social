31a6c175dcb08177313596b93f4a39e72c8ea1ff
brayden2544 <brayden2544@gmail.com>
2014-07-28 22:52:24 -0600

    TableView for connections looking awesome

--- Countdown Social/RightMenuViewController.m
+++ Countdown Social/RightMenuViewController.m
@@ -37,7 +37,8 @@
 //    self.ConnectionsTableView.delegate = self;
     // Do any additional setup after loading the view.
     //[self.ConnectionsTableView registerClass:[ConnectionsCellTableViewCell class] forCellReuseIdentifier:@"connectionCell"];
-
+    self.ConnectionsTableView.layer.cornerRadius = 5;
+    self.ConnectionsTableView.layer.masksToBounds = YES;
     
     
 }
