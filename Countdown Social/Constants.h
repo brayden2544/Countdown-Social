3cfb927d459407a92ace9878644b88752c416e63
brayden2544 <brayden2544@gmail.com>
2014-09-11 08:56:45 -0600

    Notifications fixed

--- Countdown Social/Constants.h
+++ Countdown Social/Constants.h
@@ -11,8 +11,10 @@
 @interface Constants : NSObject
 
 #ifdef DEBUG
-    #define kBaseURL @"http://api-dev.countdownsocial.com/"
+    //#define kBaseURL @"http://api-dev.countdownsocial.com/"
+    #define kBaseURL @"http://countdown-live.elasticbeanstalk.com"
 #else
+    //#define kBaseURL @"http://api-live.countdownsocial.com/";
     #define kBaseURL @"http://api-live.countdownsocial.com/";
 #endif
 
